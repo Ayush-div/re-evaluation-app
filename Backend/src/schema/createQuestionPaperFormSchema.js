e7544879086d47ad30187759cc6ab7fe5c50524d
Vigyat Singh <code.vigyat@gmail.com>
2025-02-02 21:00:46 +0530

    modified createQuestionPaperSchmea

--- Backend/src/schema/createQuestionPaperFormSchema.js
+++ Backend/src/schema/createQuestionPaperFormSchema.js
@@ -1,6 +1,45 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const createQuestionPaperFormSchema = new mongoose.Schema({
+
+const subpartOfSubpartSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    marks: {
+        type: Number,
+        required: true
+    }
+});
+
+
+const subpartSchema = new mongoose.Schema({
+    subpartText: {
+        type: String,
+        required: true
+    },
+    marks: {
+        type: Number,
+        required: true
+    },
+    subpartOfSubpart: [subpartOfSubpartSchema]
+});
+
+
+const questionSchema = new mongoose.Schema({
+    questiontext: {
+        type: String,
+        required: true
+    },
+    marks: {
+        type: Number,
+        required: true
+    },
+    subpart: [subpartSchema]
+});
+
+
+const questionPaperSchema = new mongoose.Schema({
     subjectName: {
         type: String,
         required: true,
@@ -9,7 +48,7 @@ const createQuestionPaperFormSchema = new mongoose.Schema({
         type: Date,
         required: true
     },
-    totatMarks: {
+    totalMarks: {  
         type: Number,
         required: true
     },
@@ -24,7 +63,8 @@ const createQuestionPaperFormSchema = new mongoose.Schema({
     semester: {
         type: String,
         required: true
-    }
-}, { timestamps: true })
+    },
+    questions: [questionSchema] 
+}, { timestamps: true });
 
-export const addQuestionPaper = mongoose.model("addQuestionPaper", createQuestionPaperFormSchema)
+export const QuestionPaper = mongoose.model("QuestionPaper", questionPaperSchema);
